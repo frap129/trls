851f0c061bef9d63498840cb3edd6c7764a755a9
Joe Maples <joe@maples.dev>
2024-10-14 16:07:34 -0400

    feat: Add option to cache built-aur packages

--- share/build-aur.sh
+++ share/build-aur.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+# Ensure we have proper permisions
+chown builder:builder /home/builder/aur -R
+
 # Build requested packages
 for pkg in "$@"; do
-    su builder -c "git clone https://aur.archlinux.org/$pkg.git /home/builder/aur/$pkg"
-    cd /home/builder/aur/$pkg
-    su builder -c "makepkg -s --noconfirm --skippgpcheck"
-    cp /home/builder/aur/$pkg/${pkg}*.tar.zst /aur
+    if [[ ! -d "/home/builder/aur/$pkg" ]]; then
+        # Get sources
+        su builder -c "git clone https://aur.archlinux.org/$pkg.git /home/builder/aur/$pkg"
+        cd /home/builder/aur/$pkg
+    else
+        # Update existing sources
+        cd /home/builder/aur/$pkg
+        su builder -c "git pull"
+    fi
+
+    # Check if package exists in cache before building
+    pkgver="$(grep "pkgver=" PKGBUILD | rev | cut -d'=' -f1 | rev)"
+    if find . -name "${pkg}-${pkgver}*.pkg.tar*" -print -quit | grep -q .; then
+        echo "$pkg already built"
+    else
+        # Build package
+        su builder -c "makepkg -fcCs --noconfirm --skippgpcheck"
+    fi
+
+    # Copy to staging directory
+    cp /home/builder/aur/$pkg/${pkg}-${pkgver}*.pkg.tar* /aur
 done
 
